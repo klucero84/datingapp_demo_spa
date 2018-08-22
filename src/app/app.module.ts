0e681b13c291ffe0754188bc99821853a3d03e92
Kenneth Lucero <Kenneth@KennethLucero.com>
2018-08-22 16:38:39 -0700

    module 4 event emmiter, input/output, services in angular,

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
  // add all new components here
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+
+// add all new services here
+import { AuthService } from './_services/auth.service';
+
 
 @NgModule({
-    // add all new components here
+   // add all new components here
+
    declarations: [
       AppComponent,
-      ValueComponent
+      NavComponent,
+      HomeComponent,
+      RegisterComponent
    ],
    imports: [
       BrowserModule,
-      HttpClientModule
+      HttpClientModule,
+      FormsModule
    ],
-   providers: [],
+   // add all new services here
+   providers: [
+      AuthService
+   ],
    bootstrap: [
       AppComponent
    ]
