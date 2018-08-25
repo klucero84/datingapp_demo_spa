96e49d5d2b6db890e17306e04eda5daabd85bd9a
Kenneth Lucero <Kenneth@KennethLucero.com>
2018-08-25 14:38:39 -0700

    end of lecture 57 / section 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
-import { BsDropdownModule } from 'ngx-bootstrap'
+import { BsDropdownModule } from 'ngx-bootstrap';
 
  // add all new components here
 import { AppComponent } from './app.component';
