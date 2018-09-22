af03b4932d0decb0acc4074fa66ade96374683fa
Kenneth Lucero <Kenneth@KennethLucero.com>
2018-09-22 16:00:09 -0700

    added list resolver, added like to user service, added likes to member card, added likes to lists component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,6 +35,7 @@ import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
+import { ListsResolver } from './_resolvers/lists.resolver';
 
 export function tokenGetter() {
     return localStorage.getItem('token');
@@ -88,6 +89,7 @@ export function tokenGetter() {
       MemberDetailResolver,
       MemberListResolver,
       MemberEditResolver,
+      ListsResolver,
       PreventUnsavedChanges
    ],
    bootstrap: [
